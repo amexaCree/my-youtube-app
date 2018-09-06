b09197fc5dbe645f446e3f63649b210b8d3d22c5
Amy <amy.turnah@tradesamurai.com>
2018-09-06 15:13:43 +1000

    created youtube app project and modified appdelegate

---
+++ youtube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube
+//
+//  Created by Amy Turnah on 6/9/18.
+//  Copyright © 2018 Amy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
