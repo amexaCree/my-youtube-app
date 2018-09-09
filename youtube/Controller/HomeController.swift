cbb76ac78d78346189fcfba96869c6d6cfdc4941
Amy <amy.turnah@tradesamurai.com>
2018-09-09 10:05:03 +1000

    move files into view and controller group for mvc project

---
+++ youtube/Controller/HomeController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  youtube
+//
+//  Created by Amy Turnah on 6/9/18.
+//  Copyright © 2018 Amy. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    var videos: [Video] = {
+        var blankSpaceVideo = Video()
+        blankSpaceVideo.title = "Taylor Swift - Blank Space"
+        blankSpaceVideo.thumbnailImageName = "taylor_swift_blank_space"
+        return [blankSpaceVideo]
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Home"
+        
+        view.backgroundColor = UIColor.red //changed for new swift
+        collectionView?.backgroundColor = UIColor.white
+        
+        collectionView?.register(VideoCell.self, forCellWithReuseIdentifier: "cellId") //changed for new swift
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5  //video.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        // cell.backgroundColor = UIColor.red
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let height = (view.frame.width - 16 - 16) * 9 / 16
+        let heightForOthers = CGFloat(16 + 68) // for extra height from profile, title and subtitle views, calculated from constraints) // 32 for 16px padding above and below (without profileView, titleView and subtitleView)
+        return CGSize(width: view.frame.width, height: height + heightForOthers) // changed for new swift
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
+}
+
+
+
+
--- youtube/Views/VideoCell.swift
+++ youtube/Views/VideoCell.swift
@@ -1,57 +1,12 @@
 //
-//  ViewController.swift
+//  VideoCell.swift
 //  youtube
 //
-//  Created by Amy Turnah on 6/9/18.
+//  Created by Amy Turnah on 9/9/18.
 //  Copyright © 2018 Amy. All rights reserved.
 //
 
 import UIKit
-
-class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
-    
-    var videos: [Video] = {
-        var blankSpaceVideo = Video()
-        blankSpaceVideo.title = "Taylor Swift - Blank Space"
-        blankSpaceVideo.thumbnailImageName = "taylor_swift_blank_space"
-        return [blankSpaceVideo]
-    }()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        navigationItem.title = "Home"
-        
-        view.backgroundColor = UIColor.red //changed for new swift
-        collectionView?.backgroundColor = UIColor.white
-        
-        collectionView?.register(VideoCell.self, forCellWithReuseIdentifier: "cellId") //changed for new swift
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5  //video.count
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
-        // cell.backgroundColor = UIColor.red
-        
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let height = (view.frame.width - 16 - 16) * 9 / 16
-        let heightForOthers = CGFloat(16 + 68) // for extra height from profile, title and subtitle views, calculated from constraints) // 32 for 16px padding above and below (without profileView, titleView and subtitleView)
-        return CGSize(width: view.frame.width, height: height + heightForOthers) // changed for new swift
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-
-}
 
 class VideoCell: UICollectionViewCell {
     
@@ -80,7 +35,7 @@ class VideoCell: UICollectionViewCell {
         return imageView
     }()
     
-
+    
     let separatorView: UIView =  {
         let view = UIView()
         //view.backgroundColor = UIColor.black
@@ -106,7 +61,7 @@ class VideoCell: UICollectionViewCell {
         textView.textColor = UIColor.lightGray
         return textView
     }()
-
+    
     func setupViews() {
         addSubview(thumbnailImageView)
         addSubview(separatorView)
@@ -123,7 +78,7 @@ class VideoCell: UICollectionViewCell {
         addConstraintsWithFormat("V:|-16-[v0]-8-[v1(44)]-16-[v2(1)]|", views: thumbnailImageView, userProfileImageView, separatorView)
         
         addConstraintsWithFormat("H:|[v0]|", views: separatorView)
-
+        
         
         //top constraint
         //had to be changed for new swift
@@ -150,7 +105,7 @@ class VideoCell: UICollectionViewCell {
         
         //addConstraintsWithFormat("V:|-16-[v0]-16-[v1(1)]|", views: thumbnailImageView, separatorView)
         
-
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -158,7 +113,4 @@ class VideoCell: UICollectionViewCell {
     }
     
 }
-
-
-
 
